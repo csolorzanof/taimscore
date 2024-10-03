b0ca3f2f9511712645b2afce82dc2c3aa9916a36
Carlos Solorzano <csolorzanoflores@gmail.com>
2024-10-03 08:54:22 -0600

    Removed unused variables

--- src/views/Secure/ImportDataMapping.tsx
+++ src/views/Secure/ImportDataMapping.tsx
@@ -2,8 +2,6 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../../AuthProvider'
 
 const ImportDataMapping = () => {
-    const { token, importData } = useContext(AuthContext)
-
     return <div className="container"></div>
 }
 
